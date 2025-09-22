700903bf59aa4ce642040427f98758c87a377a2b
Sameer Kohli <skohli1508@gmail.com>
2025-09-22 23:28:44 +0100

    making ready for upload

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ import {
 } from './game/gameLogic.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -27,6 +27,10 @@ app.use(express.json());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: { origin: '*', methods: ['GET', 'POST'] },
+});
+
+app.get('/', (req, res) => {
+  res.send('Traveller Tracks WebSocket Server is running!');
 });
 
 // --- Initialize game state ---
